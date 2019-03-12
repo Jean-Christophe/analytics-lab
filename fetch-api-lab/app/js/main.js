42925988b5ae4547e201035a8d49ec3fe819cb5b
Jean-Christophe <jean.christophe.vetter@gmail.com>
2019-03-12 21:27:37 +0100

    Fetch API

--- fetch-api-lab/app/js/main.js
+++ fetch-api-lab/app/js/main.js
@@ -24,38 +24,90 @@ function logError(error) {
   console.log('Looks like there was a problem:', error);
 }
 
+function validateResponse(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+}
+
+function readResponseAsJSON(response) {
+  return response.json();
+}
 
 // Fetch JSON ----------
 
 function fetchJSON() {
-  // TODO
+  fetch('examples/animals.json') // 1
+  .then(validateResponse) // 2
+  .then(readResponseAsJSON) // 3
+  .then(logResult) // 4
+  .catch(logError);
 }
 const jsonButton = document.getElementById('json-btn');
 jsonButton.addEventListener('click', fetchJSON);
 
+function showImage(responseAsBlob) {
+  const container = document.getElementById('img-container');
+  const imgElem = document.createElement('img');
+  container.appendChild(imgElem);
+  const imgUrl = URL.createObjectURL(responseAsBlob);
+  imgElem.src = imgUrl;
+}
+
+function readResponseAsBlob(response) {
+  return response.blob();
+}
 
 // Fetch Image ----------
 
 function fetchImage() {
-  // TODO
+  fetch('examples/fetching.jpg')
+    .then(validateResponse)
+    .then(readResponseAsBlob)
+    .then(showImage)
+    .catch(logError);
 }
 const imgButton = document.getElementById('img-btn');
 imgButton.addEventListener('click', fetchImage);
 
+function showText(responseAsText) {
+  const message = document.getElementById('message');
+  message.textContent = responseAsText;
+}
+
+function readResponseAsText(response) {
+  return response.text();
+}
 
 // Fetch text ----------
 
 function fetchText() {
-  // TODO
+  fetch('examples/words.txt')
+  .then(validateResponse)
+  .then(readResponseAsText)
+  .then(showText)
+  .catch(logError);
 }
 const textButton = document.getElementById('text-btn');
 textButton.addEventListener('click', fetchText);
 
 
+function logSize(response) {
+  const url = response.url;
+  const size = response.headers.get('content-length');
+  console.log(`${url} is ${size} bytes`);
+}
+
 // HEAD request ----------
 
 function headRequest() {
-  // TODO
+  fetch('examples/words.txt', {
+    method: 'HEAD'
+  })
+  .then(validateResponse)
+  .then(logSize)
+  .catch(logError);
 }
 const headButton = document.getElementById('head-btn');
 headButton.addEventListener('click', headRequest);
@@ -65,7 +117,23 @@ headButton.addEventListener('click', headRequest);
 
 /* NOTE: Never send unencrypted user credentials in production! */
 function postRequest() {
-  // TODO
+  const formData = new FormData(document.getElementById('msg-form'));
+  const messageHeaders = new Headers({
+    'Content-Type': 'application/json',
+    // 'Content-Length': 'kittens' // Content-Length can't be modified!
+    'X-Custom': 'hello world',
+    // 'Y-Custom': 'this will not work' // Y-Custom is not accepted by our echo server!
+  })
+  fetch('http://localhost:5000/', {
+    method: 'POST',
+    // body: formData
+    body: JSON.stringify({ lab: 'fetch', status: 'fun' }),
+    headers: messageHeaders
+  })
+    .then(validateResponse)
+    .then(readResponseAsText)
+    .then(showText)
+    .catch(logError);
 }
 const postButton = document.getElementById('post-btn');
 postButton.addEventListener('click', postRequest);
